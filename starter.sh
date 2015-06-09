0c708631c3091e714c2e6aff40897e6b2c065056
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-06-09 22:51:15 +0900

    add

--- starter.sh
+++ starter.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+xcode-select --install
+
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+# CLI
 brew insatll \
 git \
 zsh \
@@ -15,10 +18,17 @@ go \
 peco \
 reattach-to-user-namespace \
 
+# GUI (via homebrew-cask)
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install karabiner
+brew cask install iterm2
+brew cask install google-japanese-ime
+brew cask install dropbox
+
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 go get github.com/motemen/ghq
 
 # TODO i wanna get config files via the internet
-# TODO i wanna get GUI apps via homebrew-cask
