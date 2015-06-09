e2b745218e9557820a0c7b25707a0df48c3218c8
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-06-09 23:02:14 +0900

    put the steps to install plugins for vim and tmux

--- starter.sh
+++ starter.sh
@@ -32,3 +32,14 @@ git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 go get github.com/motemen/ghq
 
 # TODO i wanna get config files via the internet
+
+
+cat <<MSG
+
+==========
+
+should install plugins for vim and tmux with following steps:
+
+1. start vim and type ':BundleInstall'
+2. start tmux and type 'prefix-key + I'
+MSG
